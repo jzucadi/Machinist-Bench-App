dbec71f779fab3ccd5d8090220618a8a08fc8fcf
James Zaccardo <jameszaccardo@gmail.com>
2026-06-23 23:10:05 -0400

    iOS app source

---
+++ MachinistsBench/App.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MachinistsBenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Machinist's Bench")
+                .font(.title)
+        }
+    }
+}
